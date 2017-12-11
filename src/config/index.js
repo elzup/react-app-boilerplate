aed2458249c164a3af3634c5b7802c3474dd4f98
elzup <guild0105@gmail.com>
2017-12-11 14:51:46 +0900

    Update env load

--- src/config/index.js
+++ src/config/index.js
@@ -1,17 +1,36 @@
 // @flow
 
 import _combineReducers from './combineReducers'
-const { NODE_ENV, REACT_APP_HOGE, REACT_APP_FUGA_URL } = process.env
+const {
+	NODE_ENV,
+	REACT_APP_FIREBASE_API_KEY,
+	REACT_APP_FIREBASE_AUTH_DOMAIN,
+	REACT_APP_FIREBASE_DATABASE_URL,
+	REACT_APP_FIREBASE_PROJECT_ID,
+	REACT_APP_FIREBASE_STORAGE_BUCKET,
+} = process.env
 
-if (!REACT_APP_HOGE || !REACT_APP_FUGA_URL || !NODE_ENV) {
+if (
+	!REACT_APP_FIREBASE_API_KEY ||
+	!REACT_APP_FIREBASE_AUTH_DOMAIN ||
+	!REACT_APP_FIREBASE_DATABASE_URL ||
+	!REACT_APP_FIREBASE_PROJECT_ID ||
+	!REACT_APP_FIREBASE_STORAGE_BUCKET ||
+	!NODE_ENV
+) {
 	console.error(process.env)
 	throw Error('Configuration not completed. must setup envioraments.')
 }
 
 type Config = {
 	+isDev: boolean,
-	+hoge: string,
-	+fugaURL: string,
+	+firebase: {
+		+apiKey: string,
+		+authDomain: string,
+		+databaseUrl: string,
+		+projectId: string,
+		+storageBucket: string,
+	},
 	+admin: {
 		+name: string,
 		+countMax: number,
@@ -34,8 +53,13 @@ const configProduction = {
 
 const config: Config = {
 	isDev,
-	hoge: REACT_APP_HOGE,
-	fugaURL: REACT_APP_FUGA_URL,
+	firebase: {
+		apiKey: REACT_APP_FIREBASE_API_KEY,
+		authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+		databaseUrl: REACT_APP_FIREBASE_DATABASE_URL,
+		projectId: REACT_APP_FIREBASE_PROJECT_ID,
+		storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+	},
 	admin: { name: '', countMax: 0 },
 	...(isDev ? configDevelopment : configProduction),
 }
